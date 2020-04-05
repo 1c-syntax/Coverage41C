42fdd27ef46e4a169f6076af0cdf993aa4d25a40
Коротовских Дмитрий <moiyasheg@gmail.com>
2020-04-05 15:56:22 +0500

    образец choco пакета

---
+++ choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = "https://github.com/proDOOMman/Coverage41C/releases/download/v$($env:chocolateyPackageVersion)/$($env:ChocolateyPackageName)-$($env:chocolateyPackageVersion).zip"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+}
+
+$covBin = Join-Path $toolsDir "\$($env:ChocolateyPackageName)-$($env:chocolateyPackageVersion)\bin"
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyPath -PathToInstall $covBin -PathType 'Machine'
+Write-Host "usage: $($env:ChocolateyPackageName) --help, after restart shell"
