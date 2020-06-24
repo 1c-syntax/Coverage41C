86f8c8658224cae28fde24d5a94df83d126f54fb
stanislav.kosolapov <s.kosolapov@42clouds.com>
2020-06-24 13:17:30 +0300

    Обновил пакет chocolatey

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -2,10 +2,14 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = "https://github.com/proDOOMman/Coverage41C/releases/download/v$($env:chocolateyPackageVersion)/$($env:ChocolateyPackageName)-$($env:chocolateyPackageVersion).zip"
 
+$checksum = "0A3D91E605E99B6462DD1B67501BD4AE2A06F27371ADB245B38B8CAFD1E7DB34"
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
+  checksumType  = "sha256"
+  checksum      = $checksum
 }
 
 $covBin = Join-Path $toolsDir "\$($env:ChocolateyPackageName)-$($env:chocolateyPackageVersion)\bin"
