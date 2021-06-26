46d19d2e32a62f7f7303f0365934157d63acb41c
Alexey Sosnoviy <int-it@yandex.ru>
2021-06-27 00:00:04 +0300

    Переосмысление строк к покрытию

--- src/main/java/com/clouds42/LinesToCoverage.java
+++ src/main/java/com/clouds42/LinesToCoverage.java
@@ -21,31 +21,71 @@
  */
 package com.clouds42;
 
+import com.github._1c_syntax.bsl.parser.BSLLexer;
 import com.github._1c_syntax.bsl.parser.BSLParser;
 import com.github._1c_syntax.bsl.parser.BSLParserRuleContext;
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.TerminalNodeImpl;
+import org.antlr.v4.runtime.tree.TerminalNode;
 import org.antlr.v4.runtime.tree.Trees;
 
+import java.util.Set;
+
 public class LinesToCoverage {
+
+    private static final Set<Class<? extends BSLParserRuleContext>> contexts = Set.of(
+            BSLParser.AssignmentContext.class,
+            BSLParser.CallStatementContext.class,
+            BSLParser.GotoStatementContext.class,
+            BSLParser.ReturnStatementContext.class,
+            BSLParser.BreakStatementContext.class,
+            BSLParser.ContinueStatementContext.class,
+            BSLParser.IfStatementContext.class,
+            BSLParser.RaiseStatementContext.class,
+            BSLParser.ForEachStatementContext.class,
+            BSLParser.ForStatementContext.class,
+            BSLParser.WhileStatementContext.class,
+            BSLParser.GlobalMethodCallContext.class
+    );
+
+    private static final Set<Integer> tokenTypes = Set.of(
+            BSLLexer.ENDDO_KEYWORD,
+            BSLLexer.ENDFUNCTION_KEYWORD,
+            BSLLexer.ENDPROCEDURE_KEYWORD,
+            BSLLexer.ENDTRY_KEYWORD,
+            BSLLexer.ENDIF_KEYWORD,
+            BSLLexer.DO_KEYWORD
+    );
 
     static int[] getLines(BSLParserRuleContext ast) {
 
         return Trees.getDescendants(ast).stream()
-                .filter(node -> !(node instanceof TerminalNodeImpl))
                 .filter(LinesToCoverage::mustCovered)
-                .mapToInt(node -> ((BSLParserRuleContext) node).getStart().getLine())
+                .mapToInt(LinesToCoverage::getLine)
                 .distinct().toArray();
     }
 
     static boolean mustCovered(ParseTree node) {
-        return (node instanceof BSLParser.StatementContext
-                && Trees.getChildren(node).stream().noneMatch(parseTree ->
-                parseTree instanceof BSLParser.PreprocessorContext
-                        || parseTree instanceof BSLParser.CompoundStatementContext
-                        && Trees.getChildren(parseTree).stream().anyMatch(
-                        child -> child instanceof BSLParser.TryStatementContext)))
-                || node instanceof BSLParser.GlobalMethodCallContext;
+
+        if (node instanceof ParserRuleContext) {
+            return contexts.contains(node.getClass());
+        } else if (node instanceof TerminalNode) {
+            return tokenTypes.contains(
+                    ((TerminalNode) node).getSymbol().getType()
+            );
+        }
+
+        throw new IllegalArgumentException();
     }
 
+    private static int getLine(ParseTree node) {
+
+        if (node instanceof ParserRuleContext) {
+            return ((ParserRuleContext) node).getStart().getLine();
+        } else if (node instanceof TerminalNode) {
+            return ((TerminalNode) node).getSymbol().getLine();
+        }
+
+        throw new IllegalArgumentException();
+    }
 }
--- src/test/java/com/clouds42/LinesToCoverageTest.java
+++ src/test/java/com/clouds42/LinesToCoverageTest.java
@@ -28,7 +28,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 class LinesToCoverageTest {
 
@@ -42,10 +43,12 @@ class LinesToCoverageTest {
 
         // Реальный Замер
         // 13, 17, 19, 22, 23, 24, 27, 29, 30, 31, 33, 34, 35, 38, 39, 41, 42, 45, 46, 50, 51, 52, 54,
-        // 56, 58, 60, 66, 67, 69, 70, 71, 73, 74, 75, 78, 79, 80, 81, 82, 84, 89, 93, 94, 98, 98
-        var expected = new int[]{13, 15, 19, 22, 23, 24, 27 ,29, 30, 33, 34, 38, 39, 41, 45, 50, 51,
-                52, 54, 58, 66, 67, 69, 70, 73, 74, 75, 78, 79, 80, 81, 93, 98};
-        assertArrayEquals(expected, linesToCover);
+        // 56, 58, 60, 66, 67, 69, 70, 71, 73, 74, 75, 78, 79, 80, 81, 82, 84, 92, 93, 94, 98, 103, 105,
+        // 112, 112
+        var expected = new int[]{ 13, 17, 19, 22, 23, 24, 27, 29, 30, 31, 33, 34, 35, 38, 39, 41, 42, 45, 46, 50, 51, 52, 54,
+                 56, 58, 60, 66, 67, 69, 70, 71, 73, 74, 75, 76, 78, 79, 80, 81, 82, 84, 92, 93, 94, 98, 103, 105,
+                 112};
+        assertThat(linesToCover, equalTo(expected));
     }
 
     @Test
@@ -56,8 +59,19 @@ class LinesToCoverageTest {
 
         int[] linesToCover = LinesToCoverage.getLines(tokenizer.getAst());
 
-        var expected = new int[]{4};
-        assertArrayEquals(expected, linesToCover);
+        var expected = new int[]{4, 6, 8};
+        assertThat(linesToCover, equalTo(expected));
     }
 
+    @Test
+    void SimpleTest() throws IOException {
+
+        var file = new File("src/test/resources/linestocoverage/simple.bsl");
+        BSLTokenizer tokenizer = new BSLTokenizer(Files.readString(file.toPath()));
+
+        int[] linesToCover = LinesToCoverage.getLines(tokenizer.getAst());
+
+        var expected = new int[]{3, 4, 8, 13, 19, 21, 24, 25, 27};
+        assertThat(linesToCover, equalTo(expected));
+    }
 }
