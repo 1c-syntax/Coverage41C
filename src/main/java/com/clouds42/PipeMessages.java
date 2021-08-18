c09d4bea54a66b0b0696d58369e24b6ebc9e664b
Alexey Sosnoviy <int-it@yandex.ru>
2021-08-18 10:18:46 +0300

    Добавление заголовком лицензии в файлы

--- src/main/java/com/clouds42/PipeMessages.java
+++ src/main/java/com/clouds42/PipeMessages.java
@@ -1,3 +1,24 @@
+/*
+ * This file is a part of Coverage41C.
+ *
+ * Copyright (c) 2020-2021
+ * Kosolapov Stanislav aka proDOOMman <prodoomman@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * Coverage41C is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * Coverage41C is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Coverage41C.
+ */
 package com.clouds42;
 
 public class PipeMessages {
