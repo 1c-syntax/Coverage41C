24d5bfe82207f267c125965a1bd5567cc3df9bfd
Alexey Sosnoviy <int-it@yandex.ru>
2022-02-23 15:08:25 +0300

    Happy new year!

--- src/test/java/com/clouds42/LinesToCoverageTest.java
+++ src/test/java/com/clouds42/LinesToCoverageTest.java
@@ -1,7 +1,7 @@
 /*
  * This file is a part of Coverage41C.
  *
- * Copyright (c) 2020-2021
+ * Copyright (c) 2020-2022
  * Kosolapov Stanislav aka proDOOMman <prodoomman@gmail.com> and contributors
  *
  * SPDX-License-Identifier: LGPL-3.0-or-later
